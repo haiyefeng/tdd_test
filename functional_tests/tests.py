f7dcbc630a1e7127edcc6805333e6bddfff2d906
haiyefeng <502607864@qq.com>
2023-05-15 00:41:38 +0800

    feat: 引入持久化数据库及功能测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,21 +1,25 @@
-import unittest
-import time
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
@@ -30,14 +34,18 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”(使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
-        self.fail("Finish the test!")
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # 页面再次更新，清单中显示了这两个待办事项
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        # 伊迪丝想知道这个网站是否会记住她的清单
+        # 她看到网站为她生成了一个唯一的URL
+        # 页面中有一些文字解说这个功能 self.fail('Finish the test!')
+        # 她访问那个URL，发现待办事项清单还在
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
-# 在这里编写视图
 def home_page(request):
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+    items = Item.objects.all()
+    return render(request, "home.html", {"items": items})
